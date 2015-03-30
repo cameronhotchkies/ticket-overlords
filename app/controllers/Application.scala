f39286f1df70312a384377cc588e9b9ebb1468df
Cameron Hotchkies <cameron@semisafe.com>
2015-03-30 16:55:06 -0700

    Update to index.html.scala to display ticket availability based on controller output

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,8 +6,8 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-//    Ok(views.html.index("Your new application is ready."))
-    Ok("A very simple response")
+    val ticketsAvailable = 1000
+    Ok(views.html.index(ticketsAvailable))
   }
 
 }
