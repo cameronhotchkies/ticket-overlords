51f26be41fe0724e60f64e7f4cd26efeab1a99c7
Cameron Hotchkies <cameron@semisafe.com>
2015-03-30 17:21:43 -0700

    Modified ticketsAvailable action to return a JSON response

--- app/controllers/Tickets.scala
+++ app/controllers/Tickets.scala
@@ -1,10 +1,15 @@
 package controllers
 
 import play.api.mvc._
+import play.api.libs.json.Json
 
 object Tickets extends Controller {
   def ticketsAvailable = Action { request =>
     val availableTickets = 1000
-    Ok(availableTickets.toString)
+
+    val result = Json.obj(
+      "result" -> "ok", 
+      "ticketQuantity" -> availableTickets)
+    Ok(result)
   }
 }
