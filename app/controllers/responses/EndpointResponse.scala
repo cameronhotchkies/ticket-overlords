5d869b09193307238c4903aad020b7c19845eadc
Cameron Hotchkies <cameron@semisafe.com>
2015-06-11 12:41:28 -0700

    Added implicit JSON formatter for EndpointResponse -- Does not compile

--- app/controllers/responses/EndpointResponse.scala
+++ app/controllers/responses/EndpointResponse.scala
@@ -1,8 +1,18 @@
 package controllers.responses
 
+import play.api.libs.json.{ Json, Format }
+
 case class ErrorResult(status: Int, message: String)
+
+object ErrorResult {
+  implicit val format: Format[ErrorResult] = Json.format[ErrorResult]
+}
 
 case class EndpointResponse(
   result: String,
   response: Option[Any],
   error: Option[ErrorResult])
+
+object EndpointResponse {
+  implicit val format: Format[EndpointResponse] = Json.format[EndpointResponse]
+}
