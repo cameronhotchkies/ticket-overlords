11dfa92f10129d9a868a73c4b22ae1ec81b3bc70
Cameron Hotchkies <cameron@semisafe.com>
2015-06-11 12:36:38 -0700

    Added implicit JSON formatter for Event model

--- app/com/semisafe/ticketoverlords/Event.scala
+++ app/com/semisafe/ticketoverlords/Event.scala
@@ -1,6 +1,7 @@
 package com.semisafe.ticketoverlords
 
 import org.joda.time.DateTime
+import play.api.libs.json.{Json, Format}
 
 case class Event(
   id: Long,
@@ -11,3 +12,7 @@ case class Event(
   city: String,
   state: String,
   country: String)
+  
+object Event {
+  implicit val format: Format[Event] = Json.format[Event]
+}
