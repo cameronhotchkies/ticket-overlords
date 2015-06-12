71c6269fe4e370b74e706a721b751e9e6f74ef62
Cameron Hotchkies <cameron@semisafe.com>
2015-06-11 20:21:28 -0700

    Added base table definition and data access signature to Event model object ** Does not compile

--- app/com/semisafe/ticketoverlords/Event.scala
+++ app/com/semisafe/ticketoverlords/Event.scala
@@ -1,7 +1,12 @@
 package com.semisafe.ticketoverlords
 
 import org.joda.time.DateTime
-import play.api.libs.json.{Json, Format}
+import play.api.libs.json.{ Json, Format }
+
+import play.api.db.slick.DatabaseConfigProvider
+import slick.driver.JdbcProfile
+import play.api.Play.current
+import play.api.db.DBApi
 
 case class Event(
   id: Option[Long],
@@ -12,7 +17,33 @@ case class Event(
   city: String,
   state: String,
   country: String)
-  
+
 object Event {
   implicit val format: Format[Event] = Json.format[Event]
+
+  protected val dbConfig = DatabaseConfigProvider.get[JdbcProfile](current)
+  import dbConfig._
+  import dbConfig.driver.api._
+
+  class EventsTable(tag: Tag) extends Table[Event](tag, "EVENTS") {
+
+    def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
+    def name = column[String]("NAME")
+    def start = column[DateTime]("START")
+    def end = column[DateTime]("END")
+    def address = column[String]("ADDRESS")
+    def city = column[String]("CITY")
+    def state = column[String]("STATE")
+    def country = column[String]("COUNTRY")
+
+    def * = ???
+  }
+
+  val table = TableQuery[EventsTable]
+
+  def list: Seq[Event] = { ??? }
+
+  def getByID(eventID: Long): Option[Event] = { ??? }
+
+  def create(event: Event): Event = { ??? }
 }
