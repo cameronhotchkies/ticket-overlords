bb18c5ac90cf1a78080b1c8db0017492cd5a0675
Cameron Hotchkies <cameron@semisafe.com>
2015-06-20 16:39:22 -0700

    Added code to TicketIssuer to support creation of TicketIssuerWorker children

--- app/com/semisafe/ticketoverlords/TicketIssuer.scala
+++ app/com/semisafe/ticketoverlords/TicketIssuer.scala
@@ -4,12 +4,24 @@ import akka.actor.Actor
 import akka.actor.Status.{ Failure => ActorFailure }
 import play.api.libs.concurrent.Execution.Implicits._
 import scala.concurrent.Future
+import akka.actor.{ ActorRef, Props }
 
 case class InsufficientTicketsAvailable(
   ticketBlockID: Long,
   ticketsAvailable: Int) extends Throwable
 
 class TicketIssuer extends Actor {
+
+  var workers = Map[Long, ActorRef]()
+
+  def createWorker(ticketBlockID: Long) {
+    if (!workers.contains(ticketBlockID)) {
+      val worker = context.actorOf(
+        Props(classOf[TicketIssuerWorker], ticketBlockID),
+        name = ticketBlockID.toString)
+      workers = workers + (ticketBlockID -> worker)
+    }
+  }
 
   def placeOrder(order: Order) {
     // This is important!!
